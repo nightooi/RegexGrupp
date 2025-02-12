24c1625eea15c18743fbff8d8656884ef8c61be2
nightooi <rafaellost2021@outlook.com>
2025-02-12 12:53:38 +0100

    "Initial commit"

---
+++ RegexGrupp/DataFetch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexGrupp
+{
+    internal class DataFetch
+    {
+        private const string _dataFile = "..\\Assets\\tempdata5-med fel.txt";
+        public StreamReader GetDataReader()
+        {
+           return new StreamReader(_dataFile);
+        }
+    }
+}
